4f6cd7c9aaaaaf64a1b1aadbed5db4dc0c1ca9a8
Michaël V. Dandrieux <mvd@cafecentral.fr>
2013-10-23 01:27:09 +0200

    le commissaire commit

--- grandcentral/master/template/snippet/header.snippet.php
+++ grandcentral/master/template/snippet/header.snippet.php
@@ -1,2 +1,2 @@
-<h1><a href="<?=$link?>"><span class="back"><?=$back?></span><span class="current"><?=$current?></span></a></h1>
+<h1><a href="<?=$link?>"><span class="back"><?=$back?></span><span class="current"><?=$current->cut('50')?></span></a></h1>
 <div class="clear"><!-- Clearing floats --></div>
