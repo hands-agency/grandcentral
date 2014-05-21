887c8beca0c7750f199473f69ce5f6c147111348
scux <sf@cafecentral.fr>
2014-05-21 14:58:07 +0200

    Bugs

--- grandcentral/core/system/_attr/attrI18n.php
+++ grandcentral/core/system/_attr/attrI18n.php
@@ -11,6 +11,19 @@ class attrI18n extends attrArray
 {
 	protected $field;
 /**
+ * Get complete item url
+ *
+ * @return	string	url
+ * @access	public
+ */
+	public function attach(_items &$item)
+	{
+		// print'<pre>';print_r($item);print'</pre>';
+		$this->params['table'] = $item->get_table();
+		$this->params['env'] = $item->get_env();
+		// $this->params['key'] = $item['key']->get();
+	}
+/**
  * Définir le type du champ à internationaliser
  * 
  * @return	string	le nom du champ
@@ -39,7 +52,21 @@ class attrI18n extends attrArray
  */
 	public function __tostring()
 	{
-		return $this->data[i(env, current)['version']['lang']->get()];
+		//print'<pre>';print_r(i(env, current)['version']['lang']);print'</pre>';
+		//print'<pre>là : ';print_r($this->data[i(env, current)['version']['lang']->get()]);print'</pre>';
+		switch (true) {
+			case is_string($this->data):
+				print'<pre>';print_r('ici');print'</pre>';
+				return $this->data;
+				break;
+			case empty($this->data):
+			case empty($this->data[i($this->params['env'], current)['version']['lang']->get()]):
+				return 'empty';
+				break;
+			default:
+				return $this->data[i($this->params['env'], current)['version']['lang']->get()];
+				break;
+		}
 	}
 /**
  * Default field attributes for Decimal	
