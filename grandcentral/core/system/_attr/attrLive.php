015264cccedbcf85993a82fc6a8f1a94e4b20381
scux <sf@cafecentral.fr>
2014-08-27 13:01:53 +0200

    attrLive

---
+++ grandcentral/core/system/_attr/attrLive.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * String formated attributes handling class
+ *
+ * @package 	Core
+ * @author		Sylvain Frigui <sf@cafecentral.fr>
+ * @access		public
+ * @link		http://www.cafecentral.fr/fr/wiki
+ */
+class attrLive extends attrBool
+{
+/**
+ * Definition index mysql
+ *
+ * @return	string	la définition mysql
+ * @access	public
+ */
+	public function mysql_index_definition()
+	{
+	//	definition
+		$definition = 'KEY `'.$this->params['key'].'` (`'.$this->params['key'].'`)';
+	//	retour
+		return $definition;
+	}
+/**
+ * Default field attributes for Status	
+ *
+ * @param	string	la variable
+ * @return	string	une string
+ * @access	public
+ */
+	public static function get_properties()
+	{
+	//	Start with the default for all properties
+		$params = parent::get_properties();
+	//	Somes specifics for this attr
+		$params['key']['value'] = 'live';
+		$params['key']['readonly'] = true;
+		unset($params['required']);
+	//	Return
+		return $params;
+	}
+}
+?>
