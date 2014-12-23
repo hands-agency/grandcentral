f02b0d3f716138faefe1eac75e4b1fa9287ab595
Michaël V. Dandrieux <mvd@cafecentral.fr>
2014-12-23 18:39:38 +0100

    Cards follow header height

---
+++ grandcentral/content/template/master/js/header.js
@@ -0,0 +1,85 @@
+/*********************************************************************************************
+/**	* Use H1 as navigation
+ 	* @author	mvd@cafecentral.fr
+**#******************************************************************************************/
+(function($)
+{
+//	Resize header
+	$('#header').on('resize', function()
+	{
+		console.log('var');
+	});
+	
+//	Prepare for fullsearch
+	$('#refine input').on('input', function()
+	{
+	//	Some vars
+		$input = $(this);
+		$drawer = $('header .admin .drawer');
+		
+		if ($input.val())
+		{
+		//	Open Drawer
+			$drawer.removeClass('closed').addClass('opened');
+		//	Prompt
+			$drawer.ajx(
+			{
+				app:'content',
+				template:'/master/snippet/options.search',
+				q:$input.val(),
+			},{
+				done:function()
+				{
+				//	Resize the header after
+					$drawer.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function()
+					{
+						height = $('header .admin').outerHeight();
+						$('#adminContent').css('padding-top', height+'px');
+					});
+				}
+			});
+		}
+	//	Close drawer
+		else $drawer.removeClass('opened').addClass('closed');
+	});
+	
+	
+//	Fullsearch
+	$('#refine input').keyup(function(e)
+	{
+	//	When return key
+    	if(e.keyCode == 13)
+		{
+	   	//	Some vars
+			$input = $(this);
+			$drawer = $('header .admin .drawer');
+		
+			if ($input.val())
+			{
+			//	Open Drawer
+				$drawer.removeClass('closed').addClass('opened');
+			//	Prompt
+				$drawer.ajx(
+				{
+					app:'content',
+					template:'/master/snippet/options.search',
+					q:$input.val(),
+					search:true,
+				},{
+					done:function()
+					{
+					//	Resize the header after
+						$drawer.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function()
+						{
+							height = $('header .admin').outerHeight();
+							$('#adminContent').css('padding-top', height+'px');
+						});
+					}
+				});
+			}
+		//	Close drawer
+			else $drawer.removeClass('opened').addClass('closed');
+	    }
+	});
+
+})(jQuery);
