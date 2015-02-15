108ba91a5f991b1c6103e60c1b6004e8ced80ab9
Michaël V. Dandrieux <mvd@cafecentral.fr>
2015-02-15 14:57:53 +0100

    fixed internal linking

--- grandcentral/sirtrevor/template/field/link.internal.html.php
+++ grandcentral/sirtrevor/template/field/link.internal.html.php
@@ -1,3 +1,3 @@
 <?php foreach ($items as $item): ?>
-	<li data-item="<?=$item->get_abbr()?>"><button>⇠ <?=$item['title']->cut(50)?></button></li>
+	<li data-item="<?=$item->get_abbr()?>"><button><?=$item['title']->cut(50)?></button></li>
 <?php endforeach ?>
