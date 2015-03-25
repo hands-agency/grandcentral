6744c49ea1cc394de9d54459ed040aa5620788b3
scux <sf@cafecentral.fr>
2015-03-25 17:16:29 +0100

    i18n

--- grandcentral/field/system/fieldI18n.php
+++ grandcentral/field/system/fieldI18n.php
@@ -12,6 +12,7 @@ class fieldI18n extends _fields
 {
 	protected $datatype = array('array');
 	protected $field;
+	protected $attr;
 /**
  * Définir le type du champ à internationaliser
  * 
@@ -31,6 +32,26 @@ class fieldI18n extends _fields
 	public function get_field()
 	{
 		return $this->field;
+	}
+/**
+ * Définir le type du champ à internationaliser
+ * 
+ * @return	string	le nom du champ
+ * @access	public
+ */
+	public function set_attr($attr)
+	{
+		$this->attr = $attr;
+	}
+/**
+ * Obtenir le type du champ
+ * 
+ * @return	string	le nom du champ
+ * @access	public
+ */
+	public function get_attr()
+	{
+		return $this->attr;
 	}
 }
 ?>
