42a9df838923696178245a52a08acff2a0f6fddf
scux <sf@cafecentral.fr>
2015-04-30 11:19:13 +0200

    Waiting for workflow

---
+++ grandcentral/workflow/system/_item/itemTrash.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The generic item of Grand Central
+ *
+ * @author	Sylvain Frigui <sf@hands.agency>
+ * @access	public
+ * @link		http://grandcentral.fr
+ */
+class itemTrash extends _items
+{
+
+}
+?>
