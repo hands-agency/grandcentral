4d8adb2c2a76f66e1c951f2eb34c00cf449a2954
Michaël V. Dandrieux <mvd@cafecentral.fr>
2015-06-10 15:18:41 +0200

    Now you can empty an array field...

--- grandcentral/field/template/array.html.php
+++ grandcentral/field/template/array.html.php
@@ -3,6 +3,7 @@
 	<span class="help"></span>
 </label>
 <div class="wrapper">
+	<input  type="hidden" name="<?= $_FIELD->get_name(); ?>" <?php if ($_FIELD->is_disabled()): ?>disabled="disabled" <?php endif ?>value="">
 	<?php if (isset($hackToTextarea)): ?><div class="flat"><p>This array format has more than one level and is not currently supported for edition</p><pre><?= json_encode($_FIELD->get_value()) ?></pre></div><?php else : ?>
 	<?php if ($_FIELD->get_descr() != null) : ?><div class="help"><?= $_FIELD->get_descr() ?></div><?php endif ?>
 	<div class="field">
