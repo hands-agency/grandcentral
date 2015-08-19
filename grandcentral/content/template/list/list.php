58debeaeb333699e2b8f64a5f1232c840228f1a7
Michaël V. Dandrieux <mvd@cafecentral.fr>
2015-08-19 23:38:49 +0200

    Fixed a bug in the app name

--- grandcentral/content/template/list/list.php
+++ grandcentral/content/template/list/list.php
@@ -31,7 +31,7 @@
 		'jquery.viewport',
 	//	'jquery.adaptive-backgrounds',
 		'color-thief',
-		'jquery.imagesLoaded'
+		'jquery.imagesloaded'
 	);
 
 /********************************************************************************************/
