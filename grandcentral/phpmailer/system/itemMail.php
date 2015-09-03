d42e058866e28f461e4e07c5ef6b20bae8fda78b
YasserSlimaniTlemcaniH <yasser.slimani@hands.agency>
2015-09-03 11:15:47 +0200

    mail

--- grandcentral/phpmailer/system/itemMail.php
+++ grandcentral/phpmailer/system/itemMail.php
@@ -23,9 +23,8 @@ class itemMail extends _items
 	
 	public function replace_text_with_data($datas)
 	{
-		$msg = $this['message']->get();
+		$msg = (string) $this['content'];
 		preg_match_all("/\[([a-zA-Z0-9=&]+)\]/", $msg, $results, PREG_SET_ORDER);
-		// print'<pre>';print_r($results);print'</pre>';
 		foreach ((array) $results as $result)
 		{
 			if (isset($datas[$result[1]]))
@@ -33,16 +32,17 @@ class itemMail extends _items
 				$msg = str_replace($result[0], $datas[$result[1]], $msg);
 			}
 		}
-		
-		return $msg;
+		//print'<pre>';print_r($msg);print'</pre>';
+		$this['content'] = $msg;
 	}
 	
-	public function sendtouser(itemHuman $human, array $datas = null)
+	public function sendtohuman(itemHuman $human, array $datas = null)
 	{
 		if ($this->exists())
 		{
 		//Create a new PHPMailer instance
 			$mail = new PHPMailer();
+			$mail->CharSet = 'UTF-8';
 			$mail->isSendmail();
 		//Set who the message is to be sent from
 			$mail->setFrom($this['from'], $this['fromname']);
@@ -67,6 +67,51 @@ class itemMail extends _items
 		//Attach an image file
 			// $mail->addAttachment('images/phpmailer_mini.gif');
 
+			//send the message, check for errors
+			if (!$mail->send())
+			{
+			    $validation = "Mailer Error: " . $mail->ErrorInfo;
+			} else {
+			    $validation =  "Message sent!";
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public function send()
+	{
+		if ($this->exists())
+		{
+		//Create a new PHPMailer instance
+			$mail = new PHPMailer();
+			$mail->CharSet = 'UTF-8';
+			$mail->isSendmail();
+		//Set who the message is to be sent from
+			$mail->setFrom($this['fromemail'], $this['fromname']);
+		//Set an alternative reply-to address
+			$mail->addReplyTo($this['fromemail'], $this['fromname']);
+		//Set who the message is to be sent to
+			// $mail->addAddress($user['key'], $user['title']);
+			$mail->addAddress($this['toemail'], $this['toname']);
+		//Set the subject line
+			$mail->Subject = htmlspecialchars_decode($this['subject'], ENT_HTML5);
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+			// $mail->msgHTML('<p>Login : '.$user['key'].'</p><p>Password : '.$password.'</p>');
+			$txt = nl2br(htmlspecialchars_decode((string) $this['content'], ENT_HTML5));
+			
+			$mail->msgHTML($txt);
+		//Replace the plain text body with one created manually
+			// $mail->AltBody = '
+			// Login : '.$user['key'].'
+			// Password : '.$password.'
+			// ';
+		//Attach an image file
+			// $mail->addAttachment('images/phpmailer_mini.gif');
+			
 			//send the message, check for errors
 			if (!$mail->send())
 			{
