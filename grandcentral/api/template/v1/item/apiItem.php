7ef73bb4189039bec4f3f7f4549cb01787ffa70d
Michaël V. Dandrieux <mvd@cafecentral.fr>
2015-09-29 15:51:13 +0200

    apiItem

--- grandcentral/api/template/v1/item/apiItem.php
+++ grandcentral/api/template/v1/item/apiItem.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The generic api of Grand Central
+ * Handle items
  *
  * @author	Sylvain Frigui <sf@hands.agency>
  * @access	public
@@ -19,54 +19,140 @@ class apiItem extends _apis
 		switch ($request)
 		{
 			case 'get':
+			case 'post':
+			case 'delete':
 			//	Map the hash
-			//	$this->map_hash(array('map', 'limit'));
+				$this->map_hash(array('item', 'itemid'));
 			//	Call
-			//	$this->$request($this->param['map'], $this->param['limit']);
+				$this->$request($this->param['item'], $this->param['itemid']);
 				break;
 		}
 	}
 /**
- * Get
+ * Get one or several items
+ * @param string	The item you want to get (cast)
+ * @param int	An optional item id	to get
+ * @arg	param	array	An associated array (param[status]=live&amp;param[system]=true)
+ * @arg	field	string	A comma separated list of fields for filtering the returned data (field=id,key,title)
  * @access	public
  */
-	public function get()
+	public function get($item, $itemid = null)
 	{
-	//	Get the item(s)
-		$this->result = (empty($this->param['itemid'])) ? i($this->param['item'], all) : i($this->param['item'], $this->param['itemid']);
-	//	Get an attr or a rel
-		if (isset($this->param['attr']))
+	//	Some vars
+		$d = array();
+	
+	//	Transform commas into array
+		if (isset($_GET['field']))
 		{
-			// attr exists in object
-			if (isset($this->result[$this->param['attr']]))
+			$_GET['field'] = explode(',', $_GET['field']);
+		}
+	//	Do we have an id, params or nothing?
+		if (isset($_GET['param'])) $p = $_GET['param'];
+		else if (isset($this->param['itemid'])) $p = array('id' => $this->param['itemid']);
+		else $p = null;
+
+	//	Get one item, or all of the items
+		$data = new bunch();
+		$data->get($this->param['item'], $p);
+
+	//	Format data as a uniform array
+		foreach ($data as $i)
+		{
+		//	Open a tmp array for the item
+			$tmp = array();
+			
+		//	If we filter the fields
+			if (isset($_GET['field']))
 			{
-				$this->result = $this->result[$this->param['attr']];
-				
-				if (isset($this->param['attrid']) && is_a($this->result, 'attrRel'))
+				foreach ($_GET['field'] as $field)
 				{
-					//$this->result = 
+					if (isset($i[$field])) $tmp[$field] = $i[$field]->get();
 				}
 			}
-			
-			
-		//	A rel
-			// if (isset($this->param['attrid'])) $this->result = $this->result[$this->param['attr']]->unfold()[0];
-		//	An attr
-			// else $this->result = $this->result[$this->param['attr']];
+		//	Otherwise, store everything
+			else
+			{
+				foreach ($i as $key => $value)
+				{
+					$tmp[$key] = $value->get();
+				}
+			}
+		//	Add to the array
+			$d[] = $tmp;
 		}
+		
+	//	Meta
+		$this->result['meta'] = array(
+			'status' => 'success',
+			'count' => $data->count,
+			'item' => $item,
+		);
+	//	Data
+		$this->result['data'] = $d;
 	}
-
 /**
- * Post
+ * Post an item
+ * @param string	The item table you want to post on (cast)
+ * @param int	An optional item id for updates (123)
+ * @data	data	array	An associated array of data named after the item's table (item:"cast",cast:{status:"live"},test:true)
  * @access	public
  */
-	public function post()
+	public function post($item, $itemid = null)
 	{
-	//	New object or old one
-		$this->result = (isset($this->data['id'])) ? i($this->item, $this->data['id']) : i($this->item);
-	//	Loop through data
-		foreach ($this->data as $key => $value) $this->result[$key] = $value;
-		$this->result->save();
+	//	Some vars
+		$item = (isset($_POST['item'])) ? $_POST['item'] : trigger_error('Sorry, you need and item here', E_USER_ERROR);
+		$data = (isset($_POST[$item])) ? $_POST[$item] : trigger_error('Sorry, you need some data here', E_USER_ERROR);
+		$id = (isset($itemid)) ? $itemid : null;
+		
+	//	Use an old item or open a new one
+		$i = ($id != null) ? i($item, $id) : i($item);
+
+	//	Loop through data to update it
+		foreach ($data as $key => $value) if (isset($i[$key])) $i[$key] = $value;
+		
+	//	Save!
+		if (!isset($_POST['test'])) $i->save();
+		
+	//	Get the item in a array for return
+		$daraArray = array();
+		foreach ($i as $key => $value) $daraArray[$key] = $value->get();
+		
+	//	Meta
+		$this->result['meta'] = array(
+			'status' => 'success',
+			'item' => $item,
+		);
+	//	Data
+		$this->result['data'] = $daraArray;
+	}
+/**
+ * Delete an item
+ * @param string	The item table you want to delete (cast)
+ * @param int	The item id (123)
+ * @access	public
+ */
+	public function delete($item, $itemid)
+	{
+	//	Get the item
+		$item = i($item, $itemid);
+	//	Delete if that guy exists
+		if ($item->exists() && $item['status']->get() == 'live')
+		{
+		//	"Delete"
+			$item['status'] = 'trash';
+			$item->save();
+		//	Meta
+			$this->result['meta'] = array(
+				'status' => 'success',
+			);
+		}
+		else
+		{
+		//	Meta
+			$this->result['meta'] = array(
+				'status' => 'fail',
+			);
+		}
 	}
 }
 ?>
