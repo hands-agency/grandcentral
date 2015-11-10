40751925ad965b003dbd7e41602c90649b20817b
Michaël V. Dandrieux <mvd@cafecentral.fr>
2015-11-10 14:42:33 +0100

    Hello drop pin app

---
+++ grandcentral/droppin/system/js/dropPin.js
@@ -0,0 +1,167 @@
+/*!
+* dropPin - because image maps are icky
+* http://duncanheron.github.com/dropPin/
+*
+*/
+(function( $ ){
+
+	$.fn.dropPin = function(method) {
+
+		var defaults = {
+		fixedHeight: 500,
+		fixedWidth: 500,
+		dropPinPath: '/js/dropPin/',
+		pin: 'dropPin/defaultpin@2x.png',
+		backgroundImage: 'dropPin/access-map.png',
+		backgroundColor: '#9999CC',
+		xoffset : 10,
+		yoffset : 30, //need to change this to work out icon heigh/width then subtract margin from it
+		cursor: 'crosshair',
+		pinclass: '',
+		userevent: 'click',
+		hiddenXid: '#xcoord', //used for saving to db via hidden form field
+		hiddenYid: '#ycoord', //used for saving to db via hidden form field
+		pinX: false, //set to value if you pass pin co-ords to overirde click binding to position
+		pinY: false, //set to value if you pass pin co-ords to overirde click binding to position
+		pinDataSet: '' //array of pin coordinates for front end render
+	}
+
+
+
+	var methods = {
+		init: function(options) {
+
+			var options =  $.extend(defaults, options);
+			var thisObj = this;
+
+			this.css({'cursor' : options.cursor, 'background-color' : options.backgroundColor , 'background-image' : "url('"+options.backgroundImage+"')",'height' : options.fixedHeight , 'width' : options.fixedWidth});
+			var i = 10;
+			thisObj.on(options.userevent, function (ev) {
+
+				$('.'+options.pinclass).remove();
+
+				i = i + 10;
+				var $img = $(thisObj);
+				var offset = $img.offset();
+				var x = ev.pageX - offset.left;
+				var y = ev.pageY - offset.top;
+
+				var xval = (x - options.xoffset);
+				var yval = (y - options.yoffset);
+				var imgC = $('<img class="pin '+options.pinclass+'">');
+				imgC.css('top', yval+'px');
+				imgC.css('left', xval+'px');
+				imgC.css('z-index', i);
+				
+				imgC.attr('src',  options.pin);
+
+				imgC.appendTo(thisObj);
+				$(options.hiddenXid).val(xval);
+				$(options.hiddenYid).val(yval);
+
+				// add hidden fields - can use these to save to database
+				var hiddenCtl= $('<input type="hidden" name="hiddenpin" class="pin '+options.pinclass+'">');
+		        hiddenCtl.css('top', y);
+		        hiddenCtl.css('left', x);
+		        hiddenCtl.val(x + "#" + y);
+		        hiddenCtl.appendTo(thisObj);
+
+			});
+
+		},
+		dropMulti: function(options) {
+
+			var options =  $.extend(defaults, options);
+			var thisObj = this;
+
+			thisObj.css({'cursor' : options.cursor, 'background-color' : options.backgroundColor , 'background-image' : "url('"+options.backgroundImage+"')",'height' : options.fixedHeight , 'width' : options.fixedWidth});
+			var i = 10;
+			thisObj.on(options.userevent, function (ev) {
+
+				i = i + 10;
+				var $img = $(thisObj);
+				var offset = $img.offset();
+				var x = ev.pageX - offset.left;
+				var y = ev.pageY - offset.top;
+
+				var xval = (x - options.xoffset);
+				var yval = (y - options.yoffset);
+				var imgC = $('<img class="pin">');
+				imgC.css('top', yval+'px');
+				imgC.css('left', xval+'px');
+				imgC.css('z-index', i);
+
+				imgC.attr('src',  options.pin);
+
+				imgC.appendTo(thisObj);
+				// console.log(ev.target);
+				$(options.hiddenXid).val(xval);
+				$(options.hiddenYid).val(yval);
+
+				// add hidden fields - can use these to save to database
+				var hiddenCtl= $('<input type="hidden" name="hiddenpin" class="pin">');
+		        hiddenCtl.css('top', y);
+		        hiddenCtl.css('left', x);
+		        hiddenCtl.val(x + "#" + y);
+		        hiddenCtl.appendTo(thisObj);
+
+			});
+
+		},
+		showPin: function(options) {
+
+			var options =  $.extend(defaults, options);
+
+			this.css({'cursor' : options.cursor, 'background-color' : options.backgroundColor , 'background-image' : "url('"+options.backgroundImage+"')",'height' : options.fixedHeight , 'width' : options.fixedWidth});
+
+			var xval = (options.pinX);
+			var yval = (options.pinY);
+			var imgC = $('<img class="pin">');
+			imgC.css('top', yval+'px');
+			imgC.css('left', xval+'px');
+
+			imgC.attr('src',  options.pin);
+
+			imgC.appendTo(this);
+			$(options.hiddenXid).val(xval);
+			$(options.hiddenYid).val(yval);
+
+		},
+		showPins: function(options) {
+
+			var options =  $.extend(defaults, options);
+
+			this.css({'cursor' : options.cursor, 'background-color' : options.backgroundColor , 'background-image' : "url('"+options.backgroundImage+"')",'height' : options.fixedHeight , 'width' : options.fixedWidth});
+
+			for(var i=0; i < (options.pinDataSet).markers.length; i++)
+			{
+				var dataPin = options.pinDataSet.markers[i];
+
+				var imgC = $('<img rel="/map-content.php?id='+dataPin.id+'" class="pin '+options.pinclass+'" style="top:'+dataPin.ycoord+'px;left:'+dataPin.xcoord+'px;">');
+				imgC.attr('src',  options.pin);
+				imgC.attr('title',  dataPin.title);
+
+				imgC.appendTo(this);
+			}
+
+		}
+	};
+
+	if (methods[method]) {
+
+		return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+
+	} else if (typeof method === 'object' || !method) {
+
+		return methods.init.apply(this, arguments);
+
+	} else {
+
+		alert("method does not exist");
+
+	}
+
+
+}
+
+})( jQuery );
