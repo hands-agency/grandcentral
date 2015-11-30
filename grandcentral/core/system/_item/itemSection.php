6bfba0f90397f7006d7c4d4fd8f05436e295ae88
Sylvain Frigui <sf@MacBook-Pro-de-Sylvain.local>
2015-11-30 19:09:38 +0100

    itemSection : mew method get_tostring()

--- grandcentral/core/system/_item/itemSection.php
+++ grandcentral/core/system/_item/itemSection.php
@@ -9,6 +9,18 @@
 class itemSection extends _items
 {
 /**
+ * Return the render of a section
+ *
+ * @return	html code of the section
+ * @access	public
+ */
+	public function get_tostring()
+	{
+		$app = app($this['app']['app'], $this['app']['template'], $this['app']['param'], $this->get_env());
+		// master::bind_code($this['zone']->get(), $app->__tostring(), false, $this->get_nickname());
+		return $app->__tostring();
+	}
+/**
  * Display section
  *
  * @return	html code of the section
