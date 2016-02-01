e9f4021ae59982fe2fdcc4cf92c9541cb9fd5e06
YasserSlimaniTlemcaniH <yasser.slimani@hands.agency>
2016-02-01 10:50:24 +0100

    custom

--- grandcentral/customize/system/Customize.php
+++ grandcentral/customize/system/Customize.php
@@ -5,8 +5,14 @@ class Customize{
 	protected $root_color;
 	protected $root_declination;
 
+	protected $root_uni;
+	protected $root_bicolore;
+
 	protected $colors;
 	protected $declinations;
+
+	protected $colors_uni;
+	protected $colors_bicolore;
 
 	protected $tree = array();
 
@@ -16,25 +22,73 @@ class Customize{
 	public function getDatas()
 	{
 		$dir_color = new dir($this->root_color);
-		$dir_declination = new dir($this->root_declination);
+		// $dir_declination = new dir($this->root_declination);
+
+		$dir_uni = new dir($this->root_uni);
+		$dir_bicolore = new dir($this->root_bicolore);
 
 
 		$this->colors = $dir_color->get(true);
-		$this->declinations = $dir_declination->get(true);
+		// $this->declinations = $dir_declination->get(true);
+
+		$this->colors_uni = $dir_uni->get(true);
+		$this->colors_bicolore = $dir_bicolore->get(true);
+
+		// print '<pre>';
+		// print_r($this->colors_uni);
+		// print '</pre>';
+
+		// print '<pre>';
+		// print_r($this->colors_bicolore);
+		// print '</pre>';
+		// die;
 
 
 		// Thumbnail
-		foreach ($this->declinations as $key_declination => $declination) {
-			$this->tree[$key_declination] = array();
+		// foreach ($this->declinations as $key_declination => $declination) {
+		// 	$this->tree[$key_declination] = array();
+		// 	foreach ($declination as $key_detail => $detail) {
+		// 		$this->tree[$key_declination][$key_detail] = array();
+		// 		foreach ($detail as $key_img => $detail_img) {
+		// 			$temp = media('custom/declination/'.$key_declination.'/'.$key_detail.'/'.$detail_img->get_key());
+		// 			// $this->tree[$key_declination][$key_detail][$detail_img->get_name()] = $temp->get_url();
+		// 			$this->tree[$key_declination][$key_detail][$detail_img->get_name()] = $temp->thumbnail(720,null)->get_url();
+		// 		}
+		// 	}
+		// }
+
+		// Boucle polo uni
+		foreach ($this->colors_uni as $key_uni => $uni) {
+
+			$this->tree['uni'][$key_uni] = array();
+
+			foreach ($uni as $key_img => $detail_img) {
+				$temp = media('custom/uni/'.$key_uni.'/'.$detail_img->get_key());
+
+				$this->tree['uni'][$key_uni][$detail_img->get_name()] = $temp->get_url();
+			
+			}
+
+		}
+
+		// Boucle polo bicolore
+		foreach ($this->colors_bicolore as $key_declination => $declination) {
+			$this->tree['bicolore'][$key_declination] = array();
+
 			foreach ($declination as $key_detail => $detail) {
-				$this->tree[$key_declination][$key_detail] = array();
+
+				$this->tree['bicolore'][$key_declination][$key_detail] = array();
 				foreach ($detail as $key_img => $detail_img) {
-					$temp = media('custom/declination/'.$key_declination.'/'.$key_detail.'/'.$detail_img->get_key());
-					// $this->tree[$key_declination][$key_detail][$detail_img->get_name()] = $temp->get_url();
-					$this->tree[$key_declination][$key_detail][$detail_img->get_name()] = $temp->thumbnail(720,null)->get_url();
+					$temp = media('custom/bicolore/'.$key_declination.'/'.$key_detail.'/'.$detail_img->get_key());
+					$this->tree['bicolore'][$key_declination][$key_detail][$detail_img->get_name()] = $temp->get_url();
 				}
 			}
 		}
+		print '<pre>';
+		print_r($this->tree);
+		print '</pre>';
+		die;
+
 	}
 
 	public function __construct()
@@ -42,6 +96,12 @@ class Customize{
 		$this->root_custom = app('media')->get_templateroot('site').'/custom';
 		$this->root_color = $this->root_custom.'/color';
 		$this->root_declination = $this->root_custom.'/declination';
+
+		$this->root_uni = $this->root_custom.'/uni';
+		$this->root_bicolore = $this->root_custom.'/bicolore';
+
+		$this->tree['uni'] = array();
+		$this->tree['bicolore'] = array();
 
 		$this->sizes = array(
 			'man' => array(
@@ -55,20 +115,19 @@ class Customize{
 				'4xl' => 0,
 			),
 			'woman' => array(
-				'xs' => 0,
-				's' => 0,
-				'm' => 0,
-				'l' => 0,
-				'xl' => 0,
-				'2xl' => 0,
-				'3xl' => 0,
-				'4xl' => 0,
+				'0' => 0,
+				'1' => 0,
+				'2' => 0,
+				'3' => 0,
+				'4' => 0,
+				'5' => 0,
+				'6' => 0,
+				'7' => 0,
 			)
 		);
 		$this->finitions = array(
 			'Brodé' => 'Brodé',
-			'Cuir' => 'Cuir',
-			'Platisque' => 'Platisque'
+			'Cuir' => 'Cuir'
 		);
 
 		$this->getDatas();
