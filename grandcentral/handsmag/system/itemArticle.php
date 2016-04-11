e25df46e039851e880d30b2916b7e6573d898cea
Sylvain Frigui <sf@ll.home>
2016-04-11 09:55:21 +0200

    HandsMag : sync article and magazine

--- grandcentral/handsmag/system/itemArticle.php
+++ grandcentral/handsmag/system/itemArticle.php
@@ -2,34 +2,50 @@
 /**
  * Projects
  *
- * @author	Sylvain Frigui <sf@hands.agency>
  * @access	public
  * @link		http://grandcentral.fr
  */
 class itemArticle extends _items
 {
 /**
- * Get the jobs of a projects orderer by starting date
+ * Save item into database and sync with the magazine
  *
- * @access	public
+ * @access  public
  */
-	public function get_jobs($p = array())
+	public function save()
 	{
-	//	The sprints
-		$sprints = $this->get_sprints()->get_column('id');
+		// recherche du magazine lié avec le présent article
+		$mag = i('magazine');
+		$mag->get(array(
+			'article' => $this->get_nickname()
+		));
 
-	//	Some vars
-		$default = array(
-			'project' => $sprints,
-			'order()' => 'start ASC',
-			'status' => 'live',
-		);
+		if ($mag->exists() && $mag->get_nickname() != $this['magazine']->get())
+		{
+			$q = 'DELETE FROM `_rel` WHERE `item` = "magazine" AND `itemid` = '.$mag['id'].' AND `rel` = "article" AND `relid` = '.$this['id'];
+			$db = database::connect('site');
+			$db->query($q);
+		}
 
-	//	Merge with user params
-		$p = (!empty($p)) ? array_merge($default, $p) : $default;
-	//	Fetch the sprints
-		$jobs = i('job', $p);
-		return $jobs;
+		if (!$this['magazine']->is_empty())
+		{
+			$current = $this['magazine']->unfold();
+			$current['article']->add($this->get_nickname());
+			$current->save();
+		}
+
+		parent::save();
+	}
+/**
+ * Format date for display
+ *
+ * @access  public
+ */
+	public function format_date()
+	{
+		if ($this['date']->is_empty()) return '';
+
+		return $this['date']->format('M');
 	}
 }
 ?>
--- grandcentral/handsmag/system/itemMagazine.php
+++ grandcentral/handsmag/system/itemMagazine.php
@@ -2,12 +2,57 @@
 /**
  * Magazines
  *
- * @author	Sylvain Frigui <sf@hands.agency>
  * @access	public
  * @link		http://grandcentral.fr
  */
 class itemMagazine extends _items
 {
-  
+/**
+ * Save item into database and synchronise article
+ *
+ * @access  public
+ */
+	public function save()
+	{
+		parent::save();
+
+    // remise à zéro des champs magazine dans les articles liés
+    $articles = $this['article']->unfold();
+    $articles->get('article', array(
+      'magazine' => $this->get_nickname()
+    ));
+    if ($articles->count > 0)
+    {
+      // connexion à la base de données
+      $db = database::connect('site');
+
+      foreach ($articles as $article)
+      {
+        // suppression de la relation
+        if (!in_array($article->get_nickname(), $this['article']->get()))
+        {
+          $q = 'UPDATE article SET magazine = "" WHERE id = '.$article['id'];
+        }
+        // création de la relation
+        else
+        {
+          $q = 'UPDATE article SET magazine = "'.$this->get_nickname().'" WHERE id = '.$article['id'];
+        }
+        // exécuton de la requête
+        $db->query($q);
+      }
+    }
+	}
+/**
+ * Format date for display
+ *
+ * @access  public
+ */
+	public function format_date()
+	{
+		if ($this['date']->is_empty()) return '';
+
+		return $this['date']->format('j').' '.cst('MONTH_'.$this['date']->format('n')).' '.$this['date']->format('Y');
+	}
 }
 ?>
