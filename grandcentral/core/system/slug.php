443b55a99ace54ea833503508132dfc24d924aa5
Sylvain Frigui <sf@t.home>
2016-05-31 10:31:09 +0200

    Slug : word quote bug fix

--- grandcentral/core/system/slug.php
+++ grandcentral/core/system/slug.php
@@ -26,7 +26,7 @@ class Slug
 	private function noDiacritics($string)
 	{
 		$accents_regex = '~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i';
-    $special_cases = array( '&' => 'and', "'" => '');
+    $special_cases = array( '&' => 'and', "'" => '','’'=>' ');
     $string = mb_strtolower( trim( $string ), 'UTF-8' );
     $string = str_replace( array_keys($special_cases), array_values( $special_cases), $string );
     $string = preg_replace( $accents_regex, '$1', htmlentities( $string, ENT_QUOTES, 'UTF-8' ) );
