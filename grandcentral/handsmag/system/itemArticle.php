22d3e596c6b52441c8c8f4666938d89586a5be32
Sylvain Frigui <sf@t.home>
2016-05-31 10:32:05 +0200

    Hands Mag : new methods

--- grandcentral/handsmag/system/itemArticle.php
+++ grandcentral/handsmag/system/itemArticle.php
@@ -8,6 +8,7 @@
 class itemArticle extends _items
 {
 	protected static $topics;
+	protected $zip_dir;
 /**
  * Save item into database and sync with the magazine
  *
@@ -15,27 +16,32 @@ class itemArticle extends _items
  */
 	public function save()
 	{
+		// sauvegarde de l'item courant
+		parent::save();
 		// recherche du magazine lié avec le présent article
-		$mag = i('magazine');
-		$mag->get(array(
-			'article' => $this->get_nickname()
-		));
-
-		if ($mag->exists() && $mag->get_nickname() != $this['magazine']->get())
+		$mag = $this['magazine']->unfold();
+		$articles = array_keys($mag['article']->unfold()->set_index('id')->data);
+		// echo "<pre>";print_r($articles);echo "</pre>";
+		// mise à jour du magazine
+		if ($mag->exists() && !in_array($this->get_nickname(), $articles))
 		{
-			$q = 'DELETE FROM `_rel` WHERE `item` = "magazine" AND `itemid` = '.$mag['id'].' AND `rel` = "article" AND `relid` = '.$this['id'];
 			$db = database::connect('site');
+			// on efface la précédente entrée liée à l'article
+			$q = 'DELETE FROM `_rel` WHERE `item` = "magazine" AND `rel` = "article" AND `relid` = '.$this['id'];
+			$db->query($q);
+			// création de la nouvelle liaison
+			$q = 'INSERT INTO `_rel` (`item`,`itemid`,`key`,`rel`,`relid`,`position`) VALUES ("magazine",'.$mag['id'].',"article","article",'.$this['id'].','.count($articles).')';
 			$db->query($q);
 		}
-
-		if (!$this['magazine']->is_empty())
-		{
-			$current = $this['magazine']->unfold();
-			$current['article']->add($this->get_nickname());
-			$current->save();
-		}
-
-		parent::save();
+	}
+/**
+ * Format description
+ *
+ * @access  public
+ */
+	public function get_descr()
+	{
+		return $this['descr']->is_empty() ? $this['text']->cut('150') : $this['descr'];
 	}
 /**
  * Format date for display
@@ -50,6 +56,84 @@ class itemArticle extends _items
 		}
 		$topic = !$this['topic']->is_empty() ? self::$topics[$this['topic']->get()] : i('topic');
 		return $topic;
+	}
+/**
+ * Get export url
+ *
+ * @access  public
+ */
+	public function get_source()
+	{
+		$url = registry::get('exportzip');
+		if (!$url)
+		{
+			$url = i('page','exportzip')['url'];
+			registry::set('exportzip',$url);
+		}
+		$url .= '?item='.$this->get_nickname();
+		// return
+		return $url;
+	}
+/**
+ * make the html source
+ *
+ * @access  public
+ */
+	public function make_source()
+	{
+		$html = app('handsmag')->get_snippet('export/article',array('article' => $this));
+		// return
+		return $html;
+	}
+/**
+ * Get zip file
+ *
+ * @access  public
+ */
+	public function get_zip()
+	{
+		// vars
+		$this->zip_dir = app('cache')->get_templateroot('site').'/zip/';
+		$zip = new file($this->zip_dir.$this['title']->cut(50).'.zip');
+		// create destination dir if not exists
+		if (!is_dir($this->zip_dir))
+			mkdir($this->zip_dir, 0755);
+		// delete zip if article is has been modified
+		if ($zip->exists() && $zip->get_updated() < $this['updated']->format('Y-m-d H:i:s'))
+			$zip->delete();
+		// generate zip file
+		if (!$zip->exists())
+			$zip = $this->make_zip($zip->get_root());
+		// return
+		return $zip;
+	}
+/**
+ * Create a zip with article data and return a zip url
+ *
+ * @access  public
+ */
+	public function make_zip($root)
+	{
+		// create zip
+		$zip = new ZipArchive;
+		$t = $zip->open($root, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+
+		if ($t === true)
+		{
+			// html
+			$zip->addFromString($this['title']->cut(50).'.html', $this->make_source());
+			// images
+			foreach ($this['image']->unfold() as $image)
+			{
+				$zip->addFile($image->get_root(), $image->get_key());
+			}
+			// close
+			$zip->close();
+			// return
+			return new file($root);
+		}
+
+		return false;
 	}
 }
 ?>
--- grandcentral/handsmag/system/itemMagazine.php
+++ grandcentral/handsmag/system/itemMagazine.php
@@ -14,33 +14,21 @@ class itemMagazine extends _items
  */
 	public function save()
 	{
+		// on sauvegarde l'item courant
 		parent::save();
-
-    // remise à zéro des champs magazine dans les articles liés
+    // recherche des articles liés
     $articles = $this['article']->unfold();
-    $articles->get('article', array(
-      'magazine' => $this->get_nickname()
-    ));
+		// mise à jour des données
     if ($articles->count > 0)
     {
       // connexion à la base de données
       $db = database::connect('site');
-
-      foreach ($articles as $article)
-      {
-        // suppression de la relation
-        if (!in_array($article->get_nickname(), $this['article']->get()))
-        {
-          $q = 'UPDATE article SET magazine = "" WHERE id = '.$article['id'];
-        }
-        // création de la relation
-        else
-        {
-          $q = 'UPDATE article SET magazine = "'.$this->get_nickname().'" WHERE id = '.$article['id'];
-        }
-        // exécuton de la requête
-        $db->query($q);
-      }
+			// suppression des anciennes relations
+			$q = 'UPDATE article SET magazine = "" WHERE magazine = "'.$this->get_nickname().'"';
+			$db->query($q);
+      // création de la requête
+      $q = 'UPDATE article SET magazine = "'.$this->get_nickname().'" WHERE id IN ('.implode(',',$articles->get_column('id')).')';
+      $db->query($q);
     }
 	}
 /**
@@ -51,8 +39,140 @@ class itemMagazine extends _items
 	public function format_date()
 	{
 		if ($this['date']->is_empty()) return '';
+		return $this['date']->format('d').' '.cst('MONTH_'.$this['date']->format('n')).' '.$this['date']->format('Y');
+	}
+/**
+ * Get the previous magazine
+ *
+ * @access  public
+ */
+	public function get_previous()
+	{
+		if (!registry::get('magazine'))
+		{
+			$mags = i('magazine', all)->set_index('id');
+			registry::set('magazine', $mags);
+		}
+		else
+		{
+			$mags = registry::get('magazine');
+		}
+		$keys = array_keys($mags->data);
+		$currentIndex = array_search($this->get_nickname(), $keys);
+		switch (true)
+		{
+			case $currentIndex == 0:
+				$index = end($keys);
+				break;
+			default:
+				$index = $keys[$currentIndex - 1];
+				break;
+		}
+		return $mags[$index];
+	}
+/**
+ * Get the next magazine
+ *
+ * @access  public
+ */
+	public function get_next()
+	{
+		if (!registry::get('magazine'))
+		{
+			$mags = i('magazine', all)->set_index('id');
+			registry::set('magazine', $mags);
+		}
+		else
+		{
+			$mags = registry::get('magazine');
+		}
+		$keys = array_keys($mags->data);
+		$currentIndex = array_search($this->get_nickname(), $keys);
+		switch (true)
+		{
+			case $currentIndex == count($keys) - 1:
+				$index = $keys[0];
+				break;
+			default:
+				$index = $keys[$currentIndex + 1];
+				break;
+		}
+		return $mags[$index];
+	}
+/**
+ * Get export url
+ *
+ * @access  public
+ */
+	public function get_source()
+	{
+		$url = registry::get('exportzip');
+		if (!$url)
+		{
+			$url = i('page','exportzip')['url'];
+			registry::set('exportzip',$url);
+		}
+		$url .= '?item='.$this->get_nickname();
+		// return
+		return $url;
+	}
+/**
+ * Get zip file
+ *
+ * @access  public
+ */
+	public function get_zip()
+	{
+		// vars
+		$this->zip_dir = app('cache')->get_templateroot('site').'/zip/';
+		$zip = new file($this->zip_dir.$this['title']->cut(50).'.zip');
+		// create destination dir if not exists
+		if (!is_dir($this->zip_dir))
+			mkdir($this->zip_dir, 0755);
+		// delete zip if article is has been modified
+		if ($zip->exists() && $zip->get_updated() < $this['updated']->format('Y-m-d H:i:s'))
+			$zip->delete();
+		// generate zip file
+		if (!$zip->exists())
+			$zip = $this->make_zip($zip->get_root());
+		// return
+		return $zip;
+	}
+/**
+ * Create a zip with article data and return a zip url
+ *
+ * @access  public
+ */
+	public function make_zip($root)
+	{
+		$source = '';
+		// get all the articles
+		$articles = $this['article']->unfold();
+		// create zip
+		$zip = new ZipArchive;
+		$t = $zip->open($root, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
 
-		return $this['date']->format('j').' '.cst('MONTH_'.$this['date']->format('n')).' '.$this['date']->format('Y');
+		if ($t === true)
+		{
+			foreach ($articles as $article)
+			{
+				// html
+				$source .= $article->make_source();
+				// images
+				foreach ($article['image']->unfold() as $image)
+				{
+					$zip->addFile($image->get_root(), $image->get_key());
+				}
+			}
+			// create html file
+			$zip->addFromString($this['title']->cut(50).'.html', $source);
+			// close
+			$zip->close();
+			// return
+			return new file($root);
+		}
+
+		return false;
 	}
 }
 ?>
