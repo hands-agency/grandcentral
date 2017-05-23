95c1e9c7a494100d18fa8195d2df9eae9148e23c
victor-gabou <victor.gabou@outlook.fr>
2017-05-23 19:56:46 +0200

    item artist get events

--- grandcentral/hubber/system/itemArtist.php
+++ grandcentral/hubber/system/itemArtist.php
@@ -29,33 +29,7 @@ class itemArtist extends _items
  */
 	public function get_events()
 	{
-		// recherche dans le champ casting
-		$q = 'SELECT `event`.`id` FROM `event` WHERE `event`.`casting` LIKE "%['.$this['id']->get().']%" AND `event`.`status` = "live";';
-		$db = database::connect();
-		$r = $db->query($q);
-		$ids = null;
-		foreach ($r['data'] as $event)
-		{
-			$ids[] = $event['id'];
-		}
-		// recherche dans le champ en vedette
-		$q = 'SELECT `_rel`.`itemid` AS `id` FROM `_rel` WHERE `_rel`.`item` = "event" AND `_rel`.`rel` = "artist" AND `_rel`.`relid` = '.$this['id']->get().';';
-		$db = database::connect();
-		$r = $db->query($q);
-		foreach ($r['data'] as $event)
-		{
-			$ids[] = $event['id'];
-		}
-		// return a bunch of event
-		$events = new bunch();
-		if (count($ids) > 0)
-		{
-			$p = array(
-				'id' => $ids,
-				'order()' => 'start desc'
-			);
-			$events->get('event', $p);
-		}
+		$events = i('event', ['casting' => '%['.$this['id'].']%']);
 		return $events;
 	}
 }
