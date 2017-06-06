f1ff0e30f0867f1e75fa217aaa331909c9ed6c8c
Victor Gabou <victor.gabou@outlook.fr>
2017-06-06 18:31:23 +0200

    itemArtist: list news

--- grandcentral/hubber/system/itemArtist.php
+++ grandcentral/hubber/system/itemArtist.php
@@ -22,6 +22,26 @@ class itemArtist extends _items
 		return $title;
 	}
 	/**
+	* Obtenir le status de cet artiste
+	*
+	* @return	bunch  Retourne le bunch des events
+	* @access	public
+	*/
+	public function get_status()
+	{
+		$type = $this['type'];
+		$number = $this['number'];
+		if ($number != '0') {
+			$sup = $number == '1' ? '<sup>er</sup>' : '<sup>ème</sup>';
+			$status = $number . $sup . ' ' . $type;
+		}
+		else {
+			$status = (string) $type;
+		}
+
+		return $status;
+	}
+	/**
 	* Obtenir la liste des événements liés à cet artiste
 	*
 	* @return	bunch  Retourne le bunch des events
@@ -63,6 +83,70 @@ class itemArtist extends _items
 
 		return $displayName;
 	}
+
+	public function get_rel_by_tag($tables = [])
+  {
+    $tables = (array) $tables;
+
+    if (empty($this->tagrel))
+    {
+      $tagIds = [];
+      foreach ($this['tag'] as $nickname)
+      {
+        $t = explode('_', $nickname);
+        $tagIds[] = $t[1];
+      }
+
+      $q = 'SELECT * FROM _rel WHERE rel = "tag" AND item != "event" AND relid IN ('.implode(',',$tagIds).')';
+      $db = database::connect('site');
+      $r = $db->query($q);
+      $datas = [];
+      foreach ($r['data'] as $rel)
+      {
+        if (!empty($tables) && in_array($rel['item'], $tables))
+        {
+          $datas[$rel['item']][$rel['itemid']] = isset($datas[$rel['item']][$rel['itemid']]) ? $datas[$rel['item']][$rel['itemid']] + 1 : 1;
+        }
+
+      }
+      $this->tagrel = $datas;
+    }
+    return $this->tagrel;
+  }
+
+  public function get_gallery() {
+    $medias = new bunch();
+
+    $rels = $this->get_rel_by_tag('media');
+    $ids = array_keys($rels['media']);
+    $medias->get('media', [
+      'id' => $ids,
+      'type' => array('image','video'),
+      'order()' => 'created DESC',
+      'limit()' => 12
+    ]);
+    return $medias;
+  }
+
+  public function get_news() {
+    $news = new bunch();
+    $tables = array('media','news');
+    $rels = $this->get_rel_by_tag($tables);
+    foreach ($tables as $table)
+    {
+      if (isset($rels[$table]))
+      {
+        $p = [
+          'id' => array_keys($rels['media']),
+          'order()' => 'created DESC',
+          'limit()' => 12
+        ];
+        if ($table == 'media') $p['type'] = 'pdf';
+        $news->get($table, $p);
+      }
+    }
+    return $news;
+  }
 
 	/**
 	* Save
