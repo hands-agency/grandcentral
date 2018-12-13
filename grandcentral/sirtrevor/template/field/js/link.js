0ca3615c6339b5773760b436e26dc5d1f6382293
Sylvain Frigui <sylvain.frigui@gmail.com>
2018-12-13 20:08:17 +0100

    lint update

--- grandcentral/sirtrevor/template/field/js/link.js
+++ grandcentral/sirtrevor/template/field/js/link.js
@@ -2,7 +2,7 @@
 {
 //	Some vars
 	var template = '/field/link';
-	
+
 //	Tabs
 	$(document).on('click', '.adminContext .tabs li', function()
 	{
@@ -11,28 +11,29 @@
 		var $tabs = $admincontext.find('.tabs');
 		var $panels = $admincontext.find('.panels');
 		var currentTab = $(this).data('tab');
-		
+
 	//	Close all tabs & panels
 		$tabs.find('>li').attr('class', 'off');
 		$panels.find('>div').attr('class', 'off');
-		
+
 	//	Open the right one
 		$tabs.find('>li[data-tab="'+currentTab+'"]').attr('class', 'on');
 		$panel = $panels.find('>div[data-panel="'+currentTab+'"]');
 		$panel.attr('class', 'on');
-		
+
 	//	Init the panel (cameltoe function initCurrenttab)
 		var function_name = 'init'+currentTab.charAt(0).toUpperCase() + currentTab.slice(1);
-		window[function_name]($panel);	
+		window[function_name]($panel);
 	});
-	
+
 //	Init internal linking
 	initInternal = function(panel)
 	{
-		
+
 		// restore selection when blur input
 		$('.adminContext').on('blur', 'input[type="search"]', function()
 		{
+
 			restoreSelection(selRange);
 			// selRange = false;
 		});
@@ -43,7 +44,6 @@
 			{
 				restoreSelection(selRange);
 				var link = $(this).parent().data('item');
-				// console.log(link)
 				document.execCommand('CreateLink', false, link);
 				closeContext(template);
 				selRange = null;
@@ -65,20 +65,20 @@
 				target:$target,
 			})
 		//	Start with something
-			.trigger('input');
+			// .trigger('input');
 		});
 	}
-	
+
 //	Init external linking
 	initExternal = function(panel)
 	{
-		
+
 	//	Send external link
 		$(document).on('click', '.adminContext[data-template="/field/link"] [data-panel="external"] button.done', function()
 		{
 		//	Get the value from the iframe
 			var link = $(this).parent().find('iframe').contents().find('input').val();
-		
+
 		//	Good link
 			if(link && link.length > 0)
 			{
@@ -91,7 +91,7 @@
 			else console.log('That is not a valid URL, buddy');
 		});
 	}
-	
+
 //	Init media linking
 	initMedia = function(panel)
 	{
@@ -115,8 +115,8 @@
 			}
 		});
 	}
-	
+
 //	Open one by default
 	$('.adminContext .tabs li[data-tab="internal"]').trigger('click');
-	
-})(jQuery); 
+
+})(jQuery);
